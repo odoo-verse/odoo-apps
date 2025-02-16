f3594b5e657e807cd6729f87162ed1ff9c053e82
odoo verse <odooverse@gmail.com>
2025-02-16 19:30:11 +0630

    upload hide cost n price module

---
+++ ov_hide_cost_n_price/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Hide Product Cost And Sale Price",
+
+    'summary': "Hide Product Cost And Sale Price To Specific User",
+
+    'description': """Hide Product Cost And Sale Price To Specific User""",
+
+    'author': "Odoo VERSE",
+    'category': 'Inventory',
+    'version': '17.0.1.0.0',
+    'depends': ['base','stock'],
+    'data': [
+        'security/security.xml',
+        'views/inherit_product.xml',
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    "images": [
+        "static/description/icon.png",
+        "static/description/icon.png",
+    ],
+}
+
